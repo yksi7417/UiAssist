7aa0743acbeb3db51f0562455add0e2f6ba336bd
Anthony Si <yatkasi@gmail.com>
2022-06-15 21:14:11 -0400

    bring a message box

--- OCRLibTest/test_winexist.cpp
+++ OCRLibTest/test_winexist.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
+#include <windows.h>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+namespace Windows {
+	TEST(TestWinExist, IfWindowsExistsReturnTrue) {
+		MessageBox(NULL, L"hello world", L"OCRLib TestWindow", 0);
+		EXPECT_EQ(1, 1);
+		EXPECT_TRUE(true);
+	}
 }
+
