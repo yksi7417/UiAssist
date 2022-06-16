0eefee5dd0548ee66c4f1895a94cebc176416eaa
Anthony Si <yatkasi@gmail.com>
2022-06-15 23:40:02 -0400

    breaking change - can't compile - taking AutoHotKey logic in finding Windows into an OO design

--- OCRLib/Window.cpp
+++ OCRLib/Window.cpp
@@ -1,6 +1,99 @@
 #include "pch.h"
+#include <string.h>
+#include <windows.h>
+#include <dwmapi.h>
+#include <tchar.h>
+
 #include "OCRLib.h"
-#include <string>
+
+#define WINDOW_TEXT_SIZE 32767
+#define SEARCH_PHRASE_SIZE 1024
+
+class WindowSearch {
+public:
+	HWND IsMatch();
+	void SetCandidate(HWND aWnd);
+
+private:
+	void UpdateCandidateAttributes();
+	bool DetectWindow(HWND aWnd);
+	BOOL CALLBACK EnumParentFind(HWND aWnd, LPARAM lParam);
+	bool IsWindowCloaked(HWND aWnd);
+
+private:
+	HWND mCandidateParent;
+	bool mFindLastMatch; 
+	TCHAR mCandidateTitle[WINDOW_TEXT_SIZE];
+	TCHAR mCriterionTitle[SEARCH_PHRASE_SIZE]; // For storing the title.
+	size_t mCriterionTitleLength;             // Length of mCriterionTitle.
+};
+
+void WindowSearch::SetCandidate(HWND aWnd) 
+{
+	// For performance reasons, update the attributes only if the candidate window changed:
+	if (mCandidateParent != aWnd)
+	{
+		mCandidateParent = aWnd;
+		UpdateCandidateAttributes(); 
+	}
+}
+
+void WindowSearch::UpdateCandidateAttributes()
+{
+	if (!mCandidateParent)
+		return;
+	if (!GetWindowText(mCandidateParent, mCandidateTitle, _countof(mCandidateTitle)))
+		*mCandidateTitle = '\0'; 
+}
+
+HWND WindowSearch::IsMatch()
+{
+	if (_tcscmp(mCandidateTitle, mCriterionTitle))
+		return NULL;
+	return mCandidateParent;
+}
+
+bool WindowSearch::IsWindowCloaked(HWND aWnd)
+{
+	static auto* pfn = (decltype(&DwmGetWindowAttribute))
+		GetProcAddress(LoadLibrary(_T("dwmapi.dll")), "DwmGetWindowAttribute");
+	if (!pfn) // Windows XP or earlier.
+		return false;
+	int cloaked = 0;
+	const int DWMWA_CLOAKED = 14; // Work around SDK troubles.
+	auto result = pfn(aWnd, DWMWA_CLOAKED, &cloaked, sizeof(cloaked));
+	return SUCCEEDED(result) && cloaked; // Result is "invalid parameter" on Windows 7.
+}
+
+bool WindowSearch::DetectWindow(HWND aWnd)
+{
+	return (IsWindowVisible(aWnd) && !IsWindowCloaked(aWnd));
+}
+
+
+BOOL CALLBACK WindowSearch::EnumParentFind(HWND aWnd)
+{
+	if (!DetectWindow(aWnd)) return TRUE;
+	SetCandidate(aWnd);
+	return IsMatch() ? mFindLastMatch : TRUE;
+}
+
+
+HWND WinExist(std::string& title, HWND aAlreadyVisited[], int aAlreadyVisitedCount)
+{
+	HWND target_window;
+
+	WindowSearch ws;
+	if (!ws.SetCriteria(aSettings, aTitle, aText, aExcludeTitle, aExcludeText)) // No match is possible with these criteria.
+		return NULL;
+
+	ws.mAlreadyVisited = aAlreadyVisited;
+	ws.mAlreadyVisitedCount = aAlreadyVisitedCount;
+
+	EnumWindows(EnumParentFind, (LPARAM)&ws);
+
+	UPDATE_AND_RETURN_LAST_USED_WINDOW(ws.mFoundParent) // This also does a "return".
+}
 
 namespace OCRLib {
 
